f2185c812af3de5f154295672789012ae2067b95
Wambita Fana <wambitafana@gmail.com>
2019-04-05 16:59:59 +0300

    Add class for users and instances for account details

---
+++ user.py
@@ -0,0 +1,25 @@
+class User:
+    """
+
+    Class that will generate new instances of users
+
+    """
+
+    
+def __init__(self,account_name,username,password,confirm_password):
+
+        """
+        the __init__method helps us define properties for our objectsself.
+
+        """
+
+        self.account_name = account_name
+        self.username = username
+        self.password =password
+        self.confirm_password = confirm_password
+
+
+        """
+        arguments for our __init__method will include the following.
+
+        """
