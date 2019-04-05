06c95644a83df0950dfb001cca22bda75580eb42
Wambita Fana <wambitafana@gmail.com>
2019-04-05 17:02:27 +0300

    Add method that saves detail objects of user

--- user.py
+++ user.py
@@ -4,7 +4,16 @@ class User:
     Class that will generate new instances of users
 
     """
+    user_detail = []
 
+    def save_detail(self):
+
+        """
+        the method saves detail objects into the empty user_detail array
+        """
+        User.user_detail.append(self)
+
+    
     
 def __init__(self,account_name,username,password,confirm_password):
 
